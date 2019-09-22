de797ca178d496dd76f258374c3ba8f7f8aa5613
ckatt <ckatt@ukr.net>
2019-09-22 10:00:24 +0300

    add primitive varible

---
+++ thinkinginJavaWeekend_03/Precedence.java
@@ -0,0 +1,18 @@
+package thinkinginJavaWeekend_03;
+
+public class Precedence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 7;
+		int y = 2;
+		int z = 3;
+
+		int b = (y - 2) ;
+
+		System.out.println(" b = " + b);
+
+	}
+
+}
