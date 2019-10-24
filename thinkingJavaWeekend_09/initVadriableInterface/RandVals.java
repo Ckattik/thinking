fd8b3e98bf60b7498188092926d6b1a9d66e17c7
ckatt <ckatt@ukr.net>
2019-10-24 09:30:56 +0300

    init variables interface

---
+++ thinkingJavaWeekend_09/initVadriableInterface/RandVals.java
@@ -0,0 +1,13 @@
+package thinkingJavaWeekend_09.initVadriableInterface;
+
+import java.util.Random;
+
+//Инициализация полей интерфейсов не константными выражениями 
+
+public interface RandVals {
+	Random RAND = new Random(47);
+    int RANDOM_INT = RAND.nextInt(10);
+    long RANDOM_LONG = RAND.nextLong() * 10;
+    float RANDOM_FLOAT = RAND.nextFloat() * 10;
+    double RANDOM_DOUBLE = RAND.nextDouble() * 10;
+}
---
+++ thinkingJavaWeekend_09/initVadriableInterface/TestRandVals.java
@@ -0,0 +1,21 @@
+package thinkingJavaWeekend_09.initVadriableInterface;
+
+import myClass.Ckatt;
+
+public class TestRandVals {
+
+	public TestRandVals() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Ckatt.outConsole(RandVals.RANDOM_INT);
+		Ckatt.outConsole(RandVals.RANDOM_LONG);
+		Ckatt.outConsole(RandVals.RANDOM_FLOAT);
+		Ckatt.outConsole(RandVals.RANDOM_DOUBLE);
+		
+	}
+
+}
